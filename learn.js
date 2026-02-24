a47cb62553fa1c408e10859da04551b33112d86d
krishna <krishkrishnatrey@gmail.com>
2026-02-24 23:46:30 +0530

    Added modern GSAP landing page

--- learn.js
+++ learn.js
@@ -1,66 +1,59 @@
-let tl = gsap.timeline()
+gsap.registerPlugin(ScrollTrigger);
 
-tl.to(".loader h1",{
-y:-50,
-opacity:0,
-duration:1,
-delay:1
-})
-
-tl.to(".loader",{
-y:"-100%",
-duration:1
-})
-
-tl.to(".navbar",{
-opacity:1,
-y:0,
-duration:1
-})
-
-tl.to(".title",{
+gsap.to(".hero-title", {
 opacity:1,
 y:-20,
 duration:1
-})
+});
 
-tl.to(".subtitle",{
+gsap.to(".hero-sub", {
 opacity:1,
 y:-10,
-duration:1
-})
+duration:1,
+delay:0.3
+});
 
-tl.to(".btn",{
+gsap.to(".hero-btn", {
 opacity:1,
 scale:1.1,
-duration:0.5
-})
+duration:0.8,
+delay:0.6
+});
 
-gsap.to(".c1",{
-y:40,
-duration:3,
-repeat:-1,
-yoyo:true
-})
+gsap.utils.toArray(".card").forEach((card, i) => {
+gsap.to(card, {
+opacity:1,
+y:0,
+duration:1,
+delay:i * 0.2,
+scrollTrigger:{
+trigger:card,
+start:"top 80%"
+}
+});
+});
 
-gsap.to(".c2",{
-y:-30,
-duration:4,
-repeat:-1,
-yoyo:true
-})
+gsap.to(".section-title", {
+opacity:1,
+y:-20,
+duration:1,
+scrollTrigger:{
+trigger:".section-title",
+start:"top 80%"
+}
+});
 
-gsap.to(".c3",{
-x:30,
-duration:5,
-repeat:-1,
-yoyo:true
-})
+gsap.to(".footer", {
+opacity:1,
+duration:1,
+scrollTrigger:{
+trigger:".footer",
+start:"top 90%"
+}
+});
 
-document.querySelector(".btn").addEventListener("mouseenter",()=>{
-gsap.to(".btn",{scale:1.2,duration:0.3})
-})
-
-document.querySelector(".btn").addEventListener("mouseleave",()=>{
-gsap.to(".btn",{scale:1.1,duration:0.3})
-})
+gsap.from(".nav", {
+y:-100,
+opacity:0,
+duration:1
+});
